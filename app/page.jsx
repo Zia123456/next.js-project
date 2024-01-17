613df3b47e12d7394a965778bfdc6a3aa7f596da
Zia123456 <m.ziabakhteyari12@gmail.com>
2024-01-17 09:42:42 -0800

    Create section

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,15 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover & Share</h1>
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dolore
+        tempore quis cumque voluptas beatae nobis animi vel itaque dolor.
+      </p>
+    </section>
+  );
 };
 
 export default Home;
